81eac89904353a5a7c771468dfdac25dec2b6f97
wangqi <wangqi@xinxuan.net>
2025-02-22 14:03:00 +0800

    feature: update pf4j framework

--- carp-framework/carp-framework-pf4j/src/main/java/cn/sliew/carp/framework/pf4j/internal/CarpExtensionPoint.java
+++ carp-framework/carp-framework-pf4j/src/main/java/cn/sliew/carp/framework/pf4j/internal/CarpExtensionPoint.java
@@ -19,5 +19,25 @@ package cn.sliew.carp.framework.pf4j.internal;
 
 import org.pf4j.ExtensionPoint;
 
+/**
+ * Designates a Carp interface or abstract class as a PF4J {@link ExtensionPoint}.
+ */
 public interface CarpExtensionPoint extends ExtensionPoint {
+
+    /**
+     * Return the plugin ID this extension point implementation is associated with. Returns "default"
+     * if extension point is not associated with a plugin.
+     */
+    default String getPluginId() {
+        return ExtensionPointMetadataProvider.getPluginId(this);
+    }
+
+    /**
+     * Spinnaker extension points are typically proxied to provide some extension invocation
+     * instrumentation (logging, metrics, etc). To get the extension class type, use this method
+     * instead of {@link #getClass()}.
+     */
+    default Class<? extends CarpExtensionPoint> getExtensionClass() {
+        return ExtensionPointMetadataProvider.getExtensionClass(this);
+    }
 }
