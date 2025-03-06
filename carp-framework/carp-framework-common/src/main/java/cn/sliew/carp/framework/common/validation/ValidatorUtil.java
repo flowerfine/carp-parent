3c0453b792ca6cc9d9429d1e543ad092190f58d7
wangqi <wangqi@xinxuan.net>
2025-03-06 11:32:19 +0800

    feature: add validate util

---
+++ carp-framework/carp-framework-common/src/main/java/cn/sliew/carp/framework/common/validation/ValidatorUtil.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.sliew.carp.framework.common.validation;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+
+import java.util.Set;
+
+public enum ValidatorUtil {
+    ;
+
+    private static Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    public static <T> ValidateResult validate(T object) {
+        Set<ConstraintViolation<T>> validate = validator.validate(object);
+        if (validate.isEmpty()) {
+            return ValidateResult.success();
+        }
+
+        ValidateResult.ValidateResultBuilder builder = ValidateResult.failure();
+        for (ConstraintViolation constraintViolation : validate) {
+            builder.rootBeanClass(constraintViolation.getRootBeanClass().getSimpleName())
+                    .rootBean(constraintViolation.getRootBean());
+
+            ValidateResultItem item = ValidateResultItem.builder()
+                    .property(constraintViolation.getPropertyPath().toString())
+                    .invalidValue(constraintViolation.getInvalidValue())
+                    .message(constraintViolation.getMessage())
+                    .build();
+            builder.item(item);
+        }
+        return builder.build();
+    }
+
+}
